486ce3ee69ff9c11ee140ac6cf449e3004b8bfa0
Roxrad-Dev <roxrad.dev@gmail.com>
2025-08-22 00:28:14 +0200

    Quartz sync: Aug 22, 2025, 12:28 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Arebore",
+    pageTitleSuffix: "- a lore wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
